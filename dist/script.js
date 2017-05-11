b5ae9e525cb3fa2dcc29a7745b8e6777772fd8b3
Dmitry Petrov <dima.krutoo@yandex.ru>
2017-05-11 19:41:31 +0500

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,15 @@
-;(function() {
-	
+;(function () {
+
 'use strict';
-	
+
 var canvas = document.getElementById('canvas'),
 	ctx = canvas.getContext('2d'),
-	dungeon = new rglk.Dungeon({roomAmount: 1280, roomMinSize: 1, roomMaxSize: 1, density: 1}),
+	dungeon = new rglk.Dungeon({
+		roomAmount: 16, 
+		roomMinSize: 3, 
+		roomMaxSize: 7, 
+		density: 0
+	}),
 	pathfinder = new rglk.Pathfinder(function (x, y) {
 		if (dungeon._tiles[y] && dungeon._tiles[y][x]) {
 			return true;
@@ -18,18 +23,67 @@ var canvas = document.getElementById('canvas'),
 		}
 
 		return false;
-	});
+	}),
+	tileSize = 16,
+	path = [],
+	fov = [];
 
-document.addEventListener('DOMContentLoaded', function () {
-	// resize canvas
+function resize() {
+	// resize canvas 
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
+}
 
-	// generate dungeon
-	dungeon.generate(function (x, y, isFloor) {
+function draw() {
+	tileSize = Math.floor(Math.min(window.innerWidth, window.innerHeight) / Math.max(dungeon.width, dungeon.height));
+	drawMap();
+	drawFov();
+	drawPath();
+}
+
+function drawMap() {
+	dungeon.forEachTile(function (x, y, isFloor) {
 		ctx.fillStyle = isFloor ? '#445' : '#000';
-		ctx.fillRect(x * 10, y * 10, 10, 10);
+		ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
 	});
+}
+
+function drawFov() {
+	fov.forEach(function (tile) {
+		ctx.fillStyle = '#aa7';
+		ctx.fillRect(tile.x * tileSize, tile.y * tileSize, tileSize, tileSize);
+
+		ctx.fillStyle = '#333';
+		for (var iy = tile.y - 1; iy <= tile.y + 1; iy++) {
+			for (var ix = tile.x - 1; ix <= tile.x + 1; ix++) {
+				if (dungeon._tiles[iy] && dungeon._tiles[iy][ix] === false) {
+					ctx.fillRect(ix * tileSize, iy * tileSize, tileSize, tileSize);
+				}
+			}
+		}
+	});
+}
+
+function drawPath() {
+	ctx.strokeStyle = '#0f0';
+	ctx.lineWidth = Math.ceil(tileSize / 2);
+
+	ctx.beginPath();
+	ctx.moveTo(path[0].x * tileSize + (tileSize / 2), path[0].y * tileSize + (tileSize / 2));
+	path.forEach(function (point) {
+		ctx.lineTo(point.x * tileSize + (tileSize / 2), point.y * tileSize + (tileSize / 2));
+	});
+	ctx.stroke();
+	ctx.closePath();
+}
+
+window.addEventListener('resize', resize, false);
+
+document.addEventListener('DOMContentLoaded', function () {
+	resize();
+
+	// generate dungeon 
+	dungeon.generate();
 
 	// calculate fov
 	explorer.calculate(
@@ -37,39 +91,19 @@ document.addEventListener('DOMContentLoaded', function () {
 		dungeon.rooms[0].center.y, 
 		7,
 		function (x, y) {
-			ctx.fillStyle = '#aa7';
-			ctx.fillRect(x * 10, y * 10, 10, 10);
-
-			ctx.fillStyle = '#333';
-			for (var iy = y - 1; iy <= y + 1; iy++) {
-				for (var ix = x - 1; ix <= x + 1; ix++) {
-					if (dungeon._tiles[iy] && dungeon._tiles[iy][ix] === false) {
-						ctx.fillRect(ix * 10, iy * 10, 10, 10);
-					}
-				}
-			}
+			fov.push({x: x, y: y});
 		}
 	);
 
 	// search path
-	console.time('1');
-	var path = pathfinder.search(
+	path = pathfinder.search(
 		dungeon.rooms[0].center.x, 
 		dungeon.rooms[0].center.y, 
 		dungeon.rooms[dungeon.rooms.length - 1].center.x, 
 		dungeon.rooms[dungeon.rooms.length - 1].center.y
 	);
-	console.timeEnd('1');
-	
-	// draw path
-	ctx.beginPath();
-	ctx.strokeStyle = '#0f0';
-	ctx.lineWidth = 4;
-	ctx.moveTo(path[0].x * 10 + 5, path[0].y * 10 + 5);
-	path.forEach(function (point) {
-		ctx.lineTo(point.x * 10 + 5, point.y * 10 + 5);
-	});
-	ctx.stroke();
+
+	draw();
 }, false);
 
 }());
