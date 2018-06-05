99118838b41ef2be6501bf9cfb5c559d107e2203
Dmitry Petrov <dima.krutoo@yandex.ru>
2018-06-05 20:59:27 +0500

    Config updated for demo page building

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,50 +4,55 @@ import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 
-const libraryName = 'rglk',
-	outputFileName = 'rglk.js',
-	outputDirectoryName = 'build';
-
 const getConfig = (env, options) => {
 	const isProduction = Boolean(options.mode === 'production');
-	console.log(isProduction);
 	const config = {
-			entry: './src/index.js',
-			output: {
-				library: libraryName,
-				libraryTarget: 'umd',
-				path: path.join(__dirname, outputDirectoryName),
-				filename: `js/${outputFileName}`,
-			},
-			watch: !isProduction,
-			devtool: 'source-map',
-			module: {
-				rules: [
-					{
-						test: /\.js$/,
-						loader: 'babel-loader',
-						options: {
-							sourceMap: true,
-						},
-					},
-					{
-						test: /\.scss$/,
-						loader: ExtractTextPlugin.extract({
-							fallback: 'style-loader',
-							use: 'css-loader!sass-loader',
-						}),
-					}
-				],
-			},
-			plugins: [
-				new ExtractTextPlugin('css/bundle.css', {
-					allChunks: true,
-				}),
+		entry: {
+			rglk: './src/library/js/index.js',
+			demo: [
+				'./src/demo/js/index.js',
+				'./src/demo/scss/index.scss',
 			],
-			optimization: {
-				minimize: isProduction,
+		},
+		output: {
+			path: path.join(__dirname, '/build/'),
+			filename: 'js/[name].js',
+		},
+		watch: !isProduction,
+		devtool: 'source-map',
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/,
+					options: {
+						sourceMap: true,
+					},
+				},
+				{
+					test: /\.scss$/,
+					loader: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: 'css-loader!sass-loader',
+					}),
+				}
+			],
+		},
+		resolve: {
+			alias: {
+				'rglk': path.join(__dirname, '/src/library/js/index.js'),
 			}
-		};
+		},
+		plugins: [
+			new ExtractTextPlugin('css/[name].css', {
+				allChunks: true,
+			}),
+		],
+		optimization: {
+			minimize: isProduction,
+		},
+	};
 	if (isProduction) {
 		config.plugins.push(
 			new OptimizeCssAssetsPlugin(),
